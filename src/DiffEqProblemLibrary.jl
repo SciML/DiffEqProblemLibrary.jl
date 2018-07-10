76065be16768cd51a9245822fc2703153603bd3f
YingboMa <mayingbo5@gmail.com>
2018-07-09 17:15:30 -0700

    Remove `at eval`

--- src/DiffEqProblemLibrary.jl
+++ src/DiffEqProblemLibrary.jl
@@ -3,23 +3,23 @@ __precompile__(true)
 module DiffEqProblemLibrary
 
 module ODEProblemLibrary
-importodeproblems() = @eval include(joinpath(@__DIR__, "ode/ode_premade_problems.jl"))
+importodeproblems() = include(joinpath(@__DIR__, "ode/ode_premade_problems.jl"))
 end # module
 
 module DAEProblemLibrary
-importdaeproblems() = @eval include(joinpath(@__DIR__, "dae_premade_problems.jl"))
+importdaeproblems() = include(joinpath(@__DIR__, "dae_premade_problems.jl"))
 end # module
 
 module DDEProblemLibrary
-importddeproblems() = @eval include(joinpath(@__DIR__, "dde_premade_problems.jl"))
+importddeproblems() = include(joinpath(@__DIR__, "dde_premade_problems.jl"))
 end # module
 
 module SDEProblemLibrary
-importsdeproblems() = @eval include(joinpath(@__DIR__, "sde_premade_problems.jl"))
+importsdeproblems() = include(joinpath(@__DIR__, "sde_premade_problems.jl"))
 end # module
 
 module JumpProblemLibrary
-importjumpproblems() = @eval include(joinpath(@__DIR__, "jump_premade_problems.jl"))
+importjumpproblems() = include(joinpath(@__DIR__, "jump_premade_problems.jl"))
 end # module
 
 end # module
