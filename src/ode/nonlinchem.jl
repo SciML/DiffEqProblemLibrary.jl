70ca3d02d0c43c9e9961c2518bb513a517bac593
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-12-15 19:39:18 -0500

    Update nonlinchem.jl

--- src/ode/nonlinchem.jl
+++ src/ode/nonlinchem.jl
@@ -4,13 +4,35 @@ function nonLinChem(dy,y,p,t)
  dy[3] = (y[2])^2
 end
 y0 = [1.0;0.0;0.0]
-tspan = (0.0,20)
+tspan = (0.0,20.0)
 nlc_analytic(u0,p,t) = [exp(-t);
     (2sqrt(exp(-t))besselk(1,2sqrt(exp(-t)))-2besselk(1,2)/besseli(1,2)*sqrt(exp(-t))besseli(1,2sqrt(exp(-t))))/(2besselk(0,2sqrt(exp(-t)))+(2besselk(1,2)/besseli(1,2))besseli(0,2sqrt(exp(-t))));
     -exp(-t)+1+(-2sqrt(exp(-t))*besselk(1,2sqrt(exp(-t)))+sqrt(exp(-t))*besseli(1,2sqrt(exp(-t)))*2besselk(1,2)/besseli(1,2))/(2besselk(0,2sqrt(exp(-t)))+2besselk(1,2)/besseli(1,2)*besseli(0,2sqrt(exp(-t))))]
 nonLinChem_f = ODEFunction(nonLinChem,analytic = nlc_analytic)
 
 """
-TODO: Insert Problem Description here.
+Nonlinear system of reactions with an analytical solution
+
+```math
+\frac{dy_1}{dt} = -y_1
+```
+
+```math
+\frac{dy_2}{dt} = y_1 - y_2^2
+```
+
+```math
+\frac{dy_3}{dt} = y_2^2
+```
+
+with initial condition ``y=[1;0;0]`` on a time span of ``t \in (0,20)``
+
+From
+
+Liu, L. C., Tian, B., Xue, Y. S., Wang, M., & Liu, W. J. (2012). Analytic solution 
+for a nonlinear chemistry system of ordinary differential equations. Nonlinear 
+Dynamics, 68(1-2), 17-21.
+
+The analytical solution is implemented, allowing easy testing of ODE solvers.
 """
 prob_ode_nonlinchem = ODEProblem(nonLinChem,y0,tspan)
