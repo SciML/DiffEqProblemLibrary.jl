10364dbb45cfe7f7451c9a314812e174905e65ae
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-12-15 20:38:08 -0500

    Update dae_premade_problems.jl

--- src/dae_premade_problems.jl
+++ src/dae_premade_problems.jl
@@ -18,15 +18,15 @@ du0 = [-0.04, 0.04, 0.0]
 The Robertson biochemical reactions in DAE form
 
 ```math
-\\frac{dy₁}{dt} = -k₁y₁+k₃y₂y₃
+\frac{dy₁}{dt} = -k₁y₁+k₃y₂y₃
 ```
 ```math
-\\frac{dy₂}{dt} =  k₁y₁-k₂y₂^2-k₃y₂y₃
+\frac{dy₂}{dt} =  k₁y₁-k₂y₂^2-k₃y₂y₃
 ```
 ```math
 1 = y₁ + y₂ + y₃
 ```
-where ``k₁=0.04``, ``k₂=3\\times10^7``, ``k₃=10^4``. For details, see:
+where ``k₁=0.04``, ``k₂=3\times10^7``, ``k₃=10^4``. For details, see:
 Hairer Norsett Wanner Solving Ordinary Differential Equations I - Nonstiff Problems Page 129
 Usually solved on ``[0,1e11]``
 """
