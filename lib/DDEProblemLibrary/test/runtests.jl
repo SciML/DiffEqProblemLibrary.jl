6c07116beff5ecf17e8b0ff1f117e4bc83dd786b
David Widmann <david.widmann@it.uu.se>
2022-08-20 15:31:05 +0200

    Fix and test undefined exports

--- lib/DDEProblemLibrary/test/runtests.jl
+++ lib/DDEProblemLibrary/test/runtests.jl
@@ -1,2 +1,8 @@
-# The test is simply that all of the examples build!
+# The main test is simply that all of the examples build!
 using DDEProblemLibrary
+
+# Check that there are no undefined exports, stale dependencies, etc.
+# Ambiguity checks are disabled since tests fail due to ambiguities
+# in dependencies
+using Aqua
+Aqua.test_all(DDEProblemLibrary; ambiguities = false)
