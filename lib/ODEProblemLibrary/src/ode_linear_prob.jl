a46cf1be5a8ba00bc77df67339da94156ed134bf
abhro <5664668+abhro@users.noreply.github.com>
2025-09-08 08:22:19 -0400

    Add `@doc_str` macros to docstrings in ode_linear_prob.jl
    File missed in PR #163
    (https://github.com/SciML/DiffEqProblemLibrary.jl/pull/163)

--- lib/ODEProblemLibrary/src/ode_linear_prob.jl
+++ lib/ODEProblemLibrary/src/ode_linear_prob.jl
@@ -1,14 +1,14 @@
 # Linear ODE
 linear = (u, p, t) -> (p * u)
 linear_analytic = (u0, p, t) -> u0 * exp(p * t)
-"""
+@doc doc"""
 Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0=\\frac{1}{2}``, ``α=1.01``, and solution
+with initial condition ``u_0=\frac{1}{2}``, ``α=1.01``, and solution
 
 ```math
 u(t) = u_0 e^{αt}
@@ -19,14 +19,14 @@ with Float64s. The parameter is ``α``
 prob_ode_linear = ODEProblem(ODEFunction(linear, analytic = linear_analytic),
     1 / 2, (0.0, 1.0), 1.01)
 
-"""
+@doc doc"""
 Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0=\\frac{1}{2}``, ``α=1.01``, and solution
+with initial condition ``u_0=\frac{1}{2}``, ``α=1.01``, and solution
 
 ```math
 u(t) = u_0 e^{αt}
@@ -39,14 +39,14 @@ prob_ode_bigfloatlinear = ODEProblem(ODEFunction(linear, analytic = linear_analy
 
 f_2dlinear = (du, u, p, t) -> (@. du = p * u)
 f_2dlinear_analytic = (u0, p, t) -> @. u0 * exp(p * t)
-"""
+@doc doc"""
 4×2 version of the Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0`` as all uniformly distributed random numbers, 
+with initial condition ``u_0`` as all uniformly distributed random numbers,
 ``α=1.01``, and solution
 
 ```math
@@ -58,14 +58,14 @@ with Float64s
 prob_ode_2Dlinear = ODEProblem(ODEFunction(f_2dlinear, analytic = f_2dlinear_analytic),
     rand(4, 2), (0.0, 1.0), 1.01)
 
-"""
+@doc doc"""
 100×100 version of the Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0`` as all uniformly distributed random numbers, 
+with initial condition ``u_0`` as all uniformly distributed random numbers,
 ``α=1.01``, and solution
 
 ```math
@@ -78,14 +78,14 @@ prob_ode_large2Dlinear = ODEProblem(
     ODEFunction(f_2dlinear, analytic = f_2dlinear_analytic),
     rand(100, 100), (0.0, 1.0), 1.01)
 
-"""
+@doc doc"""
 4×2 version of the Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0`` as all uniformly distributed random numbers, 
+with initial condition ``u_0`` as all uniformly distributed random numbers,
 ``α=1.01``, and solution
 
 ```math
@@ -101,14 +101,14 @@ prob_ode_bigfloat2Dlinear = ODEProblem(
     big(1.01))
 
 f_2dlinear_notinplace = (u, p, t) -> p * u
-"""
+@doc doc"""
 4×2 version of the Linear ODE
 
 ```math
-\\frac{du}{dt} = αu
+\frac{du}{dt} = αu
 ```
 
-with initial condition ``u_0`` as all uniformly distributed random numbers, 
+with initial condition ``u_0`` as all uniformly distributed random numbers,
 ``α=1.01``, and solution
 
 ```math
